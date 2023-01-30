18f6480edbce479f5ced45fc78bbd422a97604b3
schavis2022 <schavis2022>
2023-01-30 14:42:57 -0500

    Init JS: Intro and Variable DONE

--- app.js
+++ app.js
@@ -4,6 +4,43 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Jumpy";
+let lastName = "Plumpy";
+var age = 51;
+
+//firstName ="Shelia"; //reassignment
+
+lastName = "Wallabeepp"
+age = 52;
+
+let language = "JavaScript";
+let createdYear = 1995;
+let InCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}.`);
+
+let product = "Jumpsuit"
+let price = 19.99;
+let isOnSale = false;
+let salePrecentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "XL";
+
+console.log(`${product} in size ${selectedSize} is $${price}
+with ${stock} in stock.`);
+
+let title = "Whispering Ghost";
+let author = "Chilly Pepper";
+let pageCount = 444;
+let bookmark = 222;
+let hasRead = true;
+
+console.log(
+  `${title} by ${author} is ${pageCount} pages long.
+Your last bookmark was page ${bookmark}.`);
+
+
 
 // Exercise 2
 
